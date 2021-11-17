dde8e6fdcd1806eecb9ed9617a3aed7ce75228fe
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-17 16:27:14 +0000

    session class

---
+++ vent/http_session.py
@@ -0,0 +1,52 @@
+import os
+import http
+import logging
+
+import requests.adapters
+import requests.packages.urllib3
+
+logger = logging.getLogger(__name__)
+
+RETRY_STRATEGY = dict(
+    total=os.getenv('RETRY_STRATEGY_TOTAL', 3),
+    status_forcelist=[
+        # 400-range
+        http.HTTPStatus.TOO_MANY_REQUESTS,
+        # 500-range
+        http.HTTPStatus.INTERNAL_SERVER_ERROR,
+        http.HTTPStatus.BAD_GATEWAY,
+        http.HTTPStatus.SERVICE_UNAVAILABLE,
+        http.HTTPStatus.GATEWAY_TIMEOUT,
+    ],
+    backoff_factor=os.getenv('RETRY_STRATEGY_BACKOFF_FACTOR', 1.1),
+)
+
+
+class GraphQLSession(requests.Session):
+    def __init__(self, token, url):
+        super().__init__()
+
+        self.headers.update({'Authorization': f'Token {token}'})
+        self.url = url
+
+    def mount_retry_strategy(self, **kwargs):
+        """
+        Auto-retry HTTP calls
+        https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/#retry-on-failure
+        """
+        retry_strategy = requests.packages.urllib3.util.retry.Retry(**kwargs)
+        adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
+
+        self.mount('https://', adapter)
+
+        return adapter
+
+    def get(self, query, **kwargs):
+        response = super().get(self.url, json=dict(query=query), **kwargs)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            logger.error(response.request.body)
+            logger.error(response.text)
+            raise
+        return response
--- vent/workflow.py
+++ vent/workflow.py
@@ -4,45 +4,13 @@ Ventilation for Hospitality workflow
 
 import logging
 import pathlib
-import http
 import datetime
 from typing import Union
 
-import requests.adapters
-import requests.packages.urllib3
-
 import vent.utils
+import vent.http_session
 
 logger = logging.getLogger(__name__)
-
-RETRY_STRATEGY = dict(
-    total=3,
-    status_forcelist=[
-        # 400-range
-        http.HTTPStatus.TOO_MANY_REQUESTS,
-        # 500-range
-        http.HTTPStatus.INTERNAL_SERVER_ERROR,
-        http.HTTPStatus.BAD_GATEWAY,
-        http.HTTPStatus.SERVICE_UNAVAILABLE,
-        http.HTTPStatus.GATEWAY_TIMEOUT,
-    ],
-    backoff_factor=1,
-)
-
-
-def mount_retry_strategy(session: requests.Session, **kwargs):
-    """
-    Auto-retry HTTP calls
-    https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/#retry-on-failure
-    """
-    retry_strategy = requests.packages.urllib3.util.retry.Retry(
-        RETRY_STRATEGY, **kwargs)
-    adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
-
-    for prefix in {'https://', 'https://'}:
-        session.mount(prefix, adapter)
-
-    return adapter
 
 
 def serialise(path: Union[str, pathlib.Path], data: str, mode: str = 'w'):
@@ -53,27 +21,15 @@ def serialise(path: Union[str, pathlib.Path], data: str, mode: str = 'w'):
         logger.info(f'Wrote "{file.name}"')
 
 
-def graphql_get(url: str, token: str, query: str, **kwargs) -> str:
-    session = requests.Session()
-    mount_retry_strategy(session)
-    session.headers.update({'Authorization': f'Token {token}'})
-    response = session.get(url, json=dict(query=query), **kwargs)
-    try:
-        response.raise_for_status()
-    except requests.HTTPError:
-        logger.error(response.request.body)
-        logger.error(response.text)
-        raise
-    return response.text
-
-
 def run(workspace_id: str, token: str, fields: str, url: str,
         time_range_start: datetime.datetime,
         time_range_end: datetime.datetime):
+    session = vent.http_session.GraphQLSession(token=token, url=url)
+
     # Download device metadata from Datacake
     device_query = vent.utils.render_template(
         './vent/templates/all_devices.j2', workspace_id=workspace_id)
-    devices = graphql_get(url=url, query=device_query, token=token)
+    devices = session.get(query=device_query).text
     serialise(path='/mnt/data/devices.json', data=devices)
 
     # Get raw data
@@ -81,7 +37,7 @@ def run(workspace_id: str, token: str, fields: str, url: str,
         './vent/templates/device_history.j2', workspace_id=workspace_id,
         fields=fields, time_range_start=time_range_start.isoformat(),
         time_range_end=time_range_end.isoformat())
-    raw_data = graphql_get(url=url, query=data_query, token=token)
+    raw_data = session.get(query=data_query).text
     serialise(path='/mnt/data/my_data.json', data=raw_data)
 
     # # Load metadata CSV from research storage
