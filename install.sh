9f06fe39ee83f3603c61a0127eb3143f2cd8b9fe
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-18 16:45:33 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+TARGET_DIR=/opt/vent
+VENV_DIR=$TARGET_DIR/venv
+PYTHON_BINARY=$VENV_DIR/bin/python3.9
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+apt-get -y install python3.9
+
+echo "Installing program files..."
+mkdir $TARGET_DIR
+cp -r ./vent $TARGET_DIR
+
+echo "Installing Python packages..."
+$PYTHON_BINARY -m pip install -r $TARGET_DIR/requirements.txt
+
+echo "Installing systemd units..."
+cp -v ./systemd/vent.service /etc/systemd/system/
+cp -v ./systemd/vent.timer /etc/systemd/system/
+systemctl daemon-reload
