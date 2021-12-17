8996963e540df9649c747c8f7b6eb946445637cd
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-12-17 11:48:48 +0000

    fix target dir permissions

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ TARGET_DIR=/opt/vent
 VENV_DIR=$TARGET_DIR/venv
 SERVICE_USER=vent
 ENV_VAR_FILE=/home/$SERVICE_USER/.env
+DATA_DIR=/mnt/airbods/ventilation
 
 # Exit immediately if a command exits with a non-zero status
 set -e
@@ -44,4 +45,6 @@ chmod 700 /home/$SERVICE_USER/.secrets
 chmod 600 /home/$SERVICE_USER/.secrets/*
 
 # Create data target
-mkdir --parents /mnt/airbods/ventilation
+mkdir --parents $DATA_DIR
+chown --recursive 1000:1000 $DATA_DIR
+chmod 774 $DATA_DIR
